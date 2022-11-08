b29e94a218df81575334673c3d16c7fba4ac4c63
Clara Chaplin <crc035@bucknell.edu>
2022-11-08 11:06:35 -0500

    made test file

---
+++ src/test/java/minesweepermvc/model/MinesweeperModelTest.java
@@ -0,0 +1,29 @@
+package minesweepermvc.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinesweeperModelTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getBoard() {
+    }
+
+    @Test
+    void getRowNumber() {
+    }
+
+    @Test
+    void getColumnNumber() {
+    }
+
+    @Test
+    void main() {
+    }
+}
